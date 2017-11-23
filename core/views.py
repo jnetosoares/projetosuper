6e0dbbc68a1d287d48ee03162c374ce6ed6ca74c
jneto.soares <jneto.soares@gmail.com>
2017-11-23 11:09:53 -0300

    Heroku teste

---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+
+def contato(request):
+    return render(request,'contato.html')
+
+
+def produto(request):
+    return render(request,'produto.html')
+
+def produtos(request):
+    return render(request,'produtos.html')
